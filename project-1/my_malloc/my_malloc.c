032cfa6469cfaf8c7d33e85971b05543782559f6
xz353 <1606283787@qq.com>
2023-01-19 14:16:20 -0500

    add split free space

--- project-1/my_malloc/my_malloc.c
+++ project-1/my_malloc/my_malloc.c
@@ -7,6 +7,8 @@ struct free_region_node_tag {
 };
 typedef struct free_region_node_tag free_node_t;
 static free_node_t head_node = {0, NULL};  //dummy head node
+static void * heap_start = NULL;
+static int program_start = 0;
 //first fit
 void * ff_malloc(size_t size) {
   free_node_t * cur = &head_node;
@@ -15,12 +17,23 @@ void * ff_malloc(size_t size) {
     prev = cur;
     cur = cur->next;
     if (cur->size >= size) {   //find region! first fit.
-      prev->next = cur->next;  //remove the node from the list.
+      prev->next = cur->next;  //remove the node from list
+      if ((int)cur->size - (int)size - (int)sizeof(free_node_t) >
+          (int)sizeof(free_node_t)) {  //enough free space, split
+        free_node_t * toAdd = (free_node_t *)(((char *)cur) + sizeof(free_node_t) + size);
+        toAdd->next = head_node.next;
+        toAdd->size = cur->size - size - 2 * sizeof(free_node_t);
+        head_node.next = toAdd;
+      }
       return ((char *)cur) + sizeof(free_node_t);
     }
   }                                                 //no free block match
   size_t total_alloc = sizeof(free_node_t) + size;  //size including data and metadata
   free_node_t * new_block = sbrk(total_alloc);
+  if (program_start == 0) {
+    heap_start = new_block;
+    program_start = 1;
+  }
   new_block->size = size;
   new_block->next = NULL;
   return ((char *)new_block) + sizeof(free_node_t);
@@ -66,3 +79,17 @@ void * bf_malloc(size_t size) {
 void bf_free(void * ptr) {
   return;
 }
+
+//test methods
+unsigned long get_data_segment_size() {
+  return sbrk(0) - heap_start;
+}  //in bytes
+unsigned long get_data_segment_free_space_size() {
+  free_node_t * cur = &head_node;
+  size_t size = 0;
+  while (cur->next != NULL) {
+    cur = cur->next;
+    size += sizeof(free_node_t) + cur->size;
+  }
+  return size;
+}  //in byte
--- project-1/my_malloc/my_malloc.h
+++ project-1/my_malloc/my_malloc.h
@@ -6,3 +6,7 @@ void ff_free(void * ptr);
 //Best Fit
 void * bf_malloc(size_t size);
 void bf_free(void * ptr);
+
+//test methods
+unsigned long get_data_segment_size();             //in bytes
+unsigned long get_data_segment_free_space_size();  //in byte
